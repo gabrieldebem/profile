65ecc18620db521d2840ed209bd8a1aabd05e53a
Gabriel Roxo <gabrieldebem72@gmail.com>
2024-10-13 19:00:31 -0300

    fix lint and readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 import { BookLayout } from './Layouts/BookLayout'
 import { Profile } from './Pages/Profile'
-import { Divider } from '@mui/material'
 import { Resume } from './Pages/Resume'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <BookLayout>
